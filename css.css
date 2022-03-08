2551161288fc07b805531289584098be83c43059
BuildTools <unconfigured@null.spigotmc.org>
2022-03-08 20:59:20 +0700

    first commit

---
+++ css.css
@@ -0,0 +1,40 @@
+img {
+    height: 100%;
+    width: 100%;
+}
+
+#outside {
+  z-index: 1;
+  position: absolute;
+  background-color: #000000;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: 1s;
+  transform-origin: 0;
+}
+
+.open-card {
+  transform: rotateY(90deg);
+}
+
+#outside-title {
+  color: #FC0;
+  font-family: Courier;
+  font-size: 1em;
+  text-align: center;
+}
+
+#outside-pic {
+  width: 40%;
+  height: 50%;
+}
+
+#inside {
+  z-index: 0;
+  position: absolute;
+  background-color: #00ABFF;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
---
+++ js.js
@@ -0,0 +1,39 @@
+function open_card() {
+    document.getElementById('outside').className = 'open-card';
+  }
+  
+  function close_card() {
+    document.getElementById('outside').className = '';
+  }
+  
+  $('.carousel').carousel({
+    interval: 50
+  });
+  
+  function err1() {
+      document.getElementById('outside-pic').src = 'https://i.pinimg.com/736x/84/a8/89/84a889054ead625422f88821ca10209d.jpg';
+  }
+  
+  function err2() {
+      document.getElementById('img1').src = 'https://pbs.twimg.com/media/C6YU7vzWYAEsncA.jpg';
+  }
+  
+  function err3() {
+      document.getElementById('img2').src = 'https://uri.org/sites/default/files/styles/hero_banner/public/media/images/2017/womens-day_650x400_61488877504.jpeg';
+  }
+  
+  function err4() {
+      document.getElementById('img3').src = 'https://pbs.twimg.com/media/CdAJ8nZVIAAxI6z.jpg';
+  }
+  
+  function err5() {
+      document.getElementById('img4').src = 'https://pbs.twimg.com/media/C6YSlTiVUAE1-eY.jpg';
+  }
+  
+  function err6() {
+      document.getElementById('img5').src = 'https://www.dobrateta.si/images/Prispevki/2014/Dan%20%C5%BEena/image1.jpg';
+  }
+  
+  function err7() {
+      document.getElementById('img6').src = 'https://i.pinimg.com/736x/1f/a3/44/1fa34408af52d74be2c3b4c263765890.jpg';
+  }
